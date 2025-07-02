d6d9a243666dbcbd94be84e19f2028c6f20d0699
VINAYMADIVAL <vinaymadivalcmp@gmail.com>
2025-07-02 22:44:47 +0530

    Updated queries.sql and add note.txt and rewrite countries.csv

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 CREATE TABLE countries(
 	id SERIAL PRIMARY KEY,
 	country_code CHAR(2) UNIQUE NOT NULL,
-	country_name VARCHAR(100)
+	country_name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE users(
@@ -12,7 +12,7 @@ color VARCHAR(15)
 
 CREATE TABLE visited_countries(
 id SERIAL PRIMARY KEY,
-country_code CHAR(2) UNIQUE NOT NULL,
+country_code CHAR(2) NOT NULL,
 user_id INTEGER REFERENCES users(id)
 );
 
@@ -26,3 +26,6 @@ SELECT *
 FROM visited_countries
 JOIN users
 ON users.id = user_id;
+
+ALTER TABLE visited_countries
+			ADD UNIQUE (user_id, country_code);
